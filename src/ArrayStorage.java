eca804a1425fa7ca2e39a17cdb89cc343b257d69
titanmet <titanmet@mail.ru>
2021-06-21 22:37:05 +0300

    dz1.1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,36 +1,52 @@
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
-        for (int i = 0; i < storage.length; i++) {
+    public void clear() {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
-    void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            storage[i] = r;
+    public void save(Resume r) {
+            storage[size] = r;
+            size++;
+    }
+
+
+    public Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume.uuid.equals(uuid))
+                return resume;
         }
-    }
-
-    Resume get(String uuid) {
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if(uuid.equals(storage[i].uuid)){
+                storage[i] = storage[size-1];
+                storage[size-1] = null;
+                size--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
