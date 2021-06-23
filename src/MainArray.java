6539afe5a4488b15a9bee412945cc4448febb837
titanmet <titanmet@mail.ru>
2021-06-23 22:34:50 +0300

    interface

--- src/MainArray.java
+++ src/MainArray.java
@@ -1,5 +1,6 @@
 import com.webapp.model.Resume;
 import com.webapp.storage.ArrayStorage;
+import com.webapp.storage.Storage;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,7 +11,7 @@ import java.io.InputStreamReader;
  * (just run, no need to understand)
  */
 public class MainArray {
-    private final static ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    private final static Storage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
