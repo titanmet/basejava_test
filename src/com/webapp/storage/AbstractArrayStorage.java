374398d6d798f27a541ed24fb06d4fc566c65eb0
titanmet <titanmet@mail.ru>
2021-06-24 22:07:30 +0300

    abstract class

---
+++ src/com/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,18 @@
+package com.webapp.storage;
+
+import com.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10000;
+    protected Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size = 0;
+
+    public int size() {
+        return size;
+    }
+}
---
+++ src/com/webapp/storage/SortedArrayStorage.java
@@ -0,0 +1,12 @@
+package com.webapp.storage;
+
+import com.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class SortedArrayStorage {
+
+}
