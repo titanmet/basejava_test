7050060c904b098be5ae565c08c11b51fdae50f8
titanmet <titanmet@mail.ru>
2021-06-29 00:21:34 +0300

    dz4 reflection, junit

--- test/com/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/webapp/storage/AbstractArrayStorageTest.java
@@ -17,7 +17,7 @@ public abstract class AbstractArrayStorageTest {
     private static final String UUID_2 = "uuid2";
     private static final Resume RESUME_2 = new Resume(UUID_2);
     private static final String UUID_3 = "uuid3";
-    public static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final Resume RESUME_3 = new Resume(UUID_3);
     private static final String UUID_4 = "uuid4";
     private static final Resume RESUME_4 = new Resume(UUID_4);
 
