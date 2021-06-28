5fe929aed23d5c4443254b468f177a7656536f84
titanmet <titanmet@mail.ru>
2021-06-29 00:44:40 +0300

    dz4 reflection, junit

--- src/com/webapp/model/Resume.java
+++ src/com/webapp/model/Resume.java
@@ -6,8 +6,6 @@ import java.util.UUID;
  * Initial resume class
  */
 public class Resume implements Comparable<Resume>{
-
-    // Unique identifier
     private final String uuid;
 
     public Resume(){
