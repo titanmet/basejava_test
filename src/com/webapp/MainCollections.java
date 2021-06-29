3dc250b2c4058bfb1f63a185ce7d710e93adb83b
titanmet <titanmet@mail.ru>
2021-06-29 21:24:47 +0300

    map

--- src/com/webapp/MainCollections.java
+++ src/com/webapp/MainCollections.java
@@ -2,20 +2,24 @@ package com.webapp;
 
 import com.webapp.model.Resume;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Objects;
+import java.util.*;
 
 public class MainCollections {
     private static final String UUID_1 = "uuid1";
-    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final Resume RESUME_1;
     private static final String UUID_2 = "uuid2";
-    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final Resume RESUME_2;
     private static final String UUID_3 = "uuid3";
-    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final Resume RESUME_3;
     private static final String UUID_4 = "uuid4";
-    private static final Resume RESUME_4 = new Resume(UUID_4);
+    private static final Resume RESUME_4;
+
+    static {
+        RESUME_1 = new Resume(UUID_1);
+        RESUME_2 = new Resume(UUID_2);
+        RESUME_3 = new Resume(UUID_3);
+        RESUME_4 = new Resume(UUID_4);
+    }
 
     public static void main(String[] args) {
         Collection<Resume> collection = new ArrayList<>();
@@ -23,21 +27,34 @@ public class MainCollections {
         collection.add(RESUME_2);
         collection.add(RESUME_3);
 
-        for(Resume r: collection){
+        for (Resume r : collection) {
             System.out.println(r);
-            if(Objects.equals(r.getUuid(), UUID_1)) {
+            if (Objects.equals(r.getUuid(), UUID_1)) {
 //                collection.remove(r);
             }
         }
 
         Iterator<Resume> iterator = collection.iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             Resume r = iterator.next();
             System.out.println(r);
-            if(Objects.equals(r.getUuid(), UUID_1)) {
+            if (Objects.equals(r.getUuid(), UUID_1)) {
                 iterator.remove();
             }
         }
         System.out.println(collection.toString());
+
+        Map<String, Resume> map = new HashMap<>();
+        map.put(UUID_1, RESUME_1);
+        map.put(UUID_2, RESUME_2);
+        map.put(UUID_3, RESUME_3);
+
+//        for (String uuid : map.keySet()) {
+//            System.out.println(map.get(uuid));
+//        }
+
+        for (Map.Entry<String, Resume> entry: map.entrySet()){
+            System.out.println(entry.getValue());
+        }
     }
 }
