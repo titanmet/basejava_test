3544f8b86d1d58e9b09bfa4b76e99c2c8afe1d7d
titanmet <titanmet@mail.ru>
2021-07-02 19:57:00 +0300

    inner class

--- src/com/webapp/MainCollections.java
+++ src/com/webapp/MainCollections.java
@@ -58,7 +58,7 @@ public class MainCollections {
         }
 
         List<Resume> resumes = Arrays.asList(RESUME_1, RESUME_2, RESUME_3);
-        resumes.remove(1);
+//        resumes.remove(1);
         System.out.println(resumes);
     }
 }
