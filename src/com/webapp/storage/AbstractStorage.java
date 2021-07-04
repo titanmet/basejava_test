cda04fb8104f54adb592a9bf478022caf87110ba
titanmet <titanmet@mail.ru>
2021-07-04 20:49:59 +0300

    logging

--- src/com/webapp/storage/AbstractStorage.java
+++ src/com/webapp/storage/AbstractStorage.java
@@ -6,8 +6,10 @@ import com.webapp.model.Resume;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
     protected abstract SK getSearchKey(String uuid);
     protected abstract void doUpdate(Resume r, SK searchKey);
     protected abstract boolean isExist(SK searchKey);
@@ -18,6 +20,7 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public void update(Resume r) {
+        LOG.info("Update " + r);
         SK searchKey = getExistedSearchKey(r.getUuid());
         doUpdate(r, searchKey);
     }
@@ -25,6 +28,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getExistedSearchKey(String uuid){
         SK searchKey = getSearchKey(uuid);
         if (!isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + " not exist");
             throw new NotExistStorageException(uuid);
         }
         return searchKey;
@@ -33,6 +37,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getNotExistedSearchKey(String uuid){
         SK searchKey = getSearchKey(uuid);
         if (isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + " already exist");
             throw new NotExistStorageException(uuid);
         }
         return searchKey;
@@ -40,18 +45,21 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public void save(Resume r) {
+        LOG.info("Save " + r);
         SK searchKey = getNotExistedSearchKey(r.getUuid());
         doSave(r, searchKey);
     }
 
     @Override
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK searchKey = getExistedSearchKey(uuid);
         return doGet(searchKey);
     }
 
     @Override
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK searchKey = getExistedSearchKey(uuid);
         doDelete(searchKey);
     }
