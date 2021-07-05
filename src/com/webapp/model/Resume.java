7959d3cd3026463dbb73299853ad0e390a1fa615
titanmet <titanmet@mail.ru>
2021-07-05 22:21:01 +0300

    dz7

--- src/com/webapp/model/Resume.java
+++ src/com/webapp/model/Resume.java
@@ -1,5 +1,9 @@
 package com.webapp.model;
 
+import sun.swing.SwingUtilities2;
+
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -10,6 +14,9 @@ public class Resume implements Comparable<Resume> {
     private final String uuid;
 
     private final String fullName;
+
+    private final Map<ContactType, String> contacts = new EnumMap<>(ContactType.class);
+    private final Map<SectionType, Section> sections = new EnumMap<>(SectionType.class);
 
     public Resume(String fullName) {
         this(UUID.randomUUID().toString(), fullName);
@@ -26,6 +33,13 @@ public class Resume implements Comparable<Resume> {
         return uuid;
     }
 
+    public String getContact(ContactType type) {
+        return contacts.get(type);
+    }
+
+    public Section getSection(SectionType type) {
+        return sections.get(type);
+    }
 
     @Override
     public String toString() {
---
+++ src/com/webapp/model/Section.java
@@ -0,0 +1,4 @@
+package com.webapp.model;
+
+abstract public class Section {
+}
