aa28f200100c64ee1d7cb2b713adaf6ee5977d42
titanmet <titanmet@mail.ru>
2021-07-12 22:01:15 +0300

    dz9

--- src/com/webapp/storage/AbstractPathStorage.java
+++ src/com/webapp/storage/AbstractPathStorage.java
@@ -12,11 +12,12 @@ import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public abstract class AbstractPathStorage extends AbstractStorage<Path> {
+public abstract class AbstractPathStorage extends AbstractPathStorage<Path> {
     private Path directory;
 
-    protected AbstractPathStorage(String dir){
+    protected AbstractPathStorage(String dir) {
         directory = Paths.get(dir);
         Objects.requireNonNull(directory, "directory must not be null");
         if (!Files.isDirectory(directory) || !Files.isWritable(directory)) {
@@ -25,84 +26,81 @@ public abstract class AbstractPathStorage extends AbstractStorage<Path> {
         this.directory = directory;
     }
 
-//    @Override
-//    protected Path getSearchKey(String uuid) {
-//        return new Path(directory, uuid);
-//    }
-
-//    @Override
-//    protected void doUpdate(Resume r, Path Path) {
-//        try {
-//            doWrite(r, new BufferedOutputStream(new PathOutputStream(Path)));
-//        } catch (IOException e) {
-//            throw new StorageException("Path write error", r.getUuid(), e);
-//        }
-//    }
-
-//    @Override
-//    protected boolean isExist(Path Path) {
-//        return Path.exists();
-//    }
-//
-//    @Override
-//    protected void doSave(Resume r, Path Path) {
-//        try {
-//            Path.createNewPath();
-//        } catch (IOException e) {
-//            throw new StorageException("Couldn't create Path " + Path.getAbsolutePath(), Path.getName(), e);
-//        }
-//        doUpdate(r, Path);
-//    }
-
-    protected abstract void doWrite(Resume r, OutputStream os) throws IOException;
-    protected abstract Resume doRead(InputStream is) throws IOException;
-
-//    @Override
-//    protected Resume doGet(Path Path) {
-//        try {
-//            return doRead(new BufferedInputStream(new PathInputStream(Path)));
-//        } catch (IOException e) {
-//            throw new StorageException("Path read error", Path.getName(), e);
-//        }
-//    }
-//
-//    @Override
-//    protected void doDelete(Path Path) {
-//        if(!Path.delete()) {
-//            throw new StorageException("Path delete error", Path.getName());
-//        }
-//    }
-//
-//    @Override
-//    protected List<Resume> doCopyAll() {
-//        Path[] Paths = directory.listPaths();
-//        if(Paths == null) {
-//            throw new StorageException("Directory read error", null);
-//        }
-//        List<Resume> list = new ArrayList<>(Paths.length);
-//        for (Path Path: Paths) {
-//            list.add(doGet(Path));
-//        }
-//        return list;
-//    }
+    @Override
+    protected Path getSearchKey(String uuid) {
+        return directory.resolve(uuid);
+    }
 
     @Override
-    public void clear() {
+    protected void doUpdate(Resume r, Path Path) {
         try {
-            List<Path> list = Files.list(directory).collect(Collectors.toList());
-            list.forEach(this::doDelete);
+            doWrite(r, new BufferedOutputStream(Files.newOutputStream(Path)));
         } catch (IOException e) {
-            throw new StorageException("Path delete error", null);
+            throw new StorageException("Path write error", r.getUuid(), e);
         }
     }
 
-//    @Override
-//    public int size() {
-//        try {
-//            Files.list(directory).forEach(this::doGet);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
+    @Override
+    protected boolean isExist(Path path) {
+        return Files.isRegularFile(path);
+    }
+
+    @Override
+    protected void doSave(Resume r, Path path) {
+        try {
+            Files.createFile(path);
+        } catch (IOException e) {
+            throw new StorageException("Couldn't create path " + path, getFileName(path), e);
+        }
+        doUpdate(r, path);
+    }
+
+    protected abstract void doWrite(Resume r, OutputStream os) throws IOException;
+
+    protected abstract Resume doRead(InputStream is) throws IOException;
+
+    @Override
+    protected Resume doGet(Path path) {
+        try {
+            return doRead(new BufferedInputStream(Files.newInputStream(path)));
+        } catch (IOException e) {
+            throw new StorageException("Path read error", getFileName(path), e);
+        }
+    }
+
+    @Override
+    protected void doDelete(Path path) {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("Path delete error", getFileName(path), e);
+        }
+    }
+
+    @Override
+    protected List<Resume> doCopyAll() {
+       return getFilesList().map(this::doGet).collect(Collectors.toList());
+    }
+
+    @Override
+    public void clear() {
+        getFilesList().forEach(this::doDelete);
+    }
+
+    @Override
+    public int size() {
+        return (int) getFilesList().count();
+    }
+
+    private String getFileName(Path path) {
+        return path.getFileName().toString();
+    }
+
+    private Stream<Path> getFilesList() {
+        try {
+            return Files.list(directory);
+        } catch (IOException e) {
+            throw new StorageException("Directory read error", e);
+        }
+    }
 }
---
+++ src/com/webapp/storage/ObjectStreamPathStorage.java
@@ -0,0 +1,28 @@
+package com.webapp.storage;
+
+import com.webapp.exception.StorageException;
+import com.webapp.model.Resume;
+
+import java.io.*;
+
+public class ObjectStreamPathStorage extends AbstractPathStorage {
+    protected ObjectStreamPathStorage(String directory) {
+        super(directory);
+    }
+
+    @Override
+    protected void doWrite(Resume r, OutputStream os) throws IOException {
+        try(ObjectOutputStream oos = new ObjectOutputStream(os)) {
+            oos.writeObject(r);
+        }
+    }
+
+    @Override
+    protected Resume doRead(InputStream is) throws IOException {
+        try(ObjectInputStream ois = new ObjectInputStream(is)) {
+            return (Resume) ois.readObject();
+        } catch (ClassNotFoundException e) {
+            throw  new StorageException("Error read resume", null, e);
+        }
+    }
+}
