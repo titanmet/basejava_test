2bad2c177efc9ac5cedf832a4219012976ee556f
titanmet <titanmet@mail.ru>
2021-07-12 20:52:24 +0300

    file directory

--- src/com/webapp/storage/AbstractPathStorage.java
+++ src/com/webapp/storage/AbstractPathStorage.java
@@ -25,66 +25,66 @@ public abstract class AbstractPathStorage extends AbstractStorage<Path> {
         this.directory = directory;
     }
 
-    @Override
-    protected Path getSearchKey(String uuid) {
-        return new Path(directory, uuid);
-    }
+//    @Override
+//    protected Path getSearchKey(String uuid) {
+//        return new Path(directory, uuid);
+//    }
 
-    @Override
-    protected void doUpdate(Resume r, Path Path) {
-        try {
-            doWrite(r, new BufferedOutputStream(new PathOutputStream(Path)));
-        } catch (IOException e) {
-            throw new StorageException("Path write error", r.getUuid(), e);
-        }
-    }
+//    @Override
+//    protected void doUpdate(Resume r, Path Path) {
+//        try {
+//            doWrite(r, new BufferedOutputStream(new PathOutputStream(Path)));
+//        } catch (IOException e) {
+//            throw new StorageException("Path write error", r.getUuid(), e);
+//        }
+//    }
 
-    @Override
-    protected boolean isExist(Path Path) {
-        return Path.exists();
-    }
-
-    @Override
-    protected void doSave(Resume r, Path Path) {
-        try {
-            Path.createNewPath();
-        } catch (IOException e) {
-            throw new StorageException("Couldn't create Path " + Path.getAbsolutePath(), Path.getName(), e);
-        }
-        doUpdate(r, Path);
-    }
+//    @Override
+//    protected boolean isExist(Path Path) {
+//        return Path.exists();
+//    }
+//
+//    @Override
+//    protected void doSave(Resume r, Path Path) {
+//        try {
+//            Path.createNewPath();
+//        } catch (IOException e) {
+//            throw new StorageException("Couldn't create Path " + Path.getAbsolutePath(), Path.getName(), e);
+//        }
+//        doUpdate(r, Path);
+//    }
 
     protected abstract void doWrite(Resume r, OutputStream os) throws IOException;
     protected abstract Resume doRead(InputStream is) throws IOException;
 
-    @Override
-    protected Resume doGet(Path Path) {
-        try {
-            return doRead(new BufferedInputStream(new PathInputStream(Path)));
-        } catch (IOException e) {
-            throw new StorageException("Path read error", Path.getName(), e);
-        }
-    }
-
-    @Override
-    protected void doDelete(Path Path) {
-        if(!Path.delete()) {
-            throw new StorageException("Path delete error", Path.getName());
-        }
-    }
-
-    @Override
-    protected List<Resume> doCopyAll() {
-        Path[] Paths = directory.listPaths();
-        if(Paths == null) {
-            throw new StorageException("Directory read error", null);
-        }
-        List<Resume> list = new ArrayList<>(Paths.length);
-        for (Path Path: Paths) {
-            list.add(doGet(Path));
-        }
-        return list;
-    }
+//    @Override
+//    protected Resume doGet(Path Path) {
+//        try {
+//            return doRead(new BufferedInputStream(new PathInputStream(Path)));
+//        } catch (IOException e) {
+//            throw new StorageException("Path read error", Path.getName(), e);
+//        }
+//    }
+//
+//    @Override
+//    protected void doDelete(Path Path) {
+//        if(!Path.delete()) {
+//            throw new StorageException("Path delete error", Path.getName());
+//        }
+//    }
+//
+//    @Override
+//    protected List<Resume> doCopyAll() {
+//        Path[] Paths = directory.listPaths();
+//        if(Paths == null) {
+//            throw new StorageException("Directory read error", null);
+//        }
+//        List<Resume> list = new ArrayList<>(Paths.length);
+//        for (Path Path: Paths) {
+//            list.add(doGet(Path));
+//        }
+//        return list;
+//    }
 
     @Override
     public void clear() {
@@ -96,13 +96,13 @@ public abstract class AbstractPathStorage extends AbstractStorage<Path> {
         }
     }
 
-    @Override
-    public int size() {
-        try {
-            Files.list(directory).forEach(this::doGet);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+//    @Override
+//    public int size() {
+//        try {
+//            Files.list(directory).forEach(this::doGet);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 }
