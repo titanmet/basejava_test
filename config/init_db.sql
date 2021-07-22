e855115213537e95f16462bf58752e66a5335f34
titanmet <titanmet@mail.ru>
2021-07-22 21:33:11 +0300

    postgres

--- config/init_db.sql
+++ config/init_db.sql
@@ -18,3 +18,6 @@ create table contact
             references resume
             on update restrict on delete cascade
 );
+
+create unique index contact_uuid_type_index
+    on contact (resume_uuid, type);
